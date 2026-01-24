4f5612c77ba3c08d88783b0ab32b89fee193e000
karthik <karthikb1010@gmail.com>
2026-01-24 07:25:45 +0530

    update

---
+++ src/maths_problems/Seive_Of_Eratosthenes.java
@@ -0,0 +1,42 @@
+package maths_problems;
+
+import java.util.Scanner;
+
+//Seive of Eratosthones--- find all primes <= N
+
+public class Seive_Of_Eratosthenes {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the number");
+		int n = sc.nextInt();
+		
+		boolean[] isPrime = new boolean[n+1];
+		
+		for( int i =2 ; i<=n ;i++) {
+			isPrime[i] = true;
+		}
+		
+		for(int i =2; i*i <=n ;i++) {
+			if (isPrime[i]) {
+				for(int j =i*i ;j<=n ;j=j+i) {
+					isPrime[j] = false;
+				}
+				
+			}
+			
+		}
+		System.out.println("Prime numbers are:" );
+		for(int i =2 ;i<=n; i++) {
+			if (isPrime[i]) {
+				System.out.println(i + " ");
+			}
+		}
+		
+		
+
+	}
+
+}
