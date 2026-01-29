e861219d0e6760347abe0b12659218535a303e82
karthik <karthikb1010@gmail.com>
2026-01-29 19:29:15 +0530

    update

--- src/_2_arrays/_G_7_Max_Subarray_Sum.java
+++ src/_2_arrays/_G_7_Max_Subarray_Sum.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 [ -2, 1, -3, 4, -1, 2, 1, -5, 4 ]
 The best subarray is:
 [4, -1, 2, 1]
-		
+
 Sum:
 4 + (-1) + 2 + 1 = 6  ✅ (maximum)
 
@@ -23,7 +23,7 @@ As we move through array:
 Because:
 
 Negative sum only makes future sum smaller.
- 
+
  */
 
 
@@ -31,29 +31,29 @@ public class _G_7_Max_Subarray_Sum {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println("Enter the array size:");
 		int n =sc.nextInt();
-		
+
 		int[] arr = new int[n];
-		
+
 		System.out.println("enter the array elements:");
 		for(int i=0;i<n;i++) {
 			arr[i] =sc.nextInt();
 		}
-		
+
 		int currentSum = arr[0];
 		int maxSum = arr[0];
-		
+
 		for(int i=1;i<n;i++) {
 			currentSum = Math.max(arr[i] ,currentSum + arr[i]);
 			maxSum = Math.max(maxSum, currentSum);
 		}
-		
-		
-			System.out.print("Maximum subarray sum = " + maxSum);
-		
-			sc.close();
+
+
+		System.out.print("Maximum subarray sum = " + maxSum);
+
+		sc.close();
 	}
 
 }
---
+++ src/_2_arrays/_H_8_Maximum_Product_Subarray.java
@@ -0,0 +1,58 @@
+package _2_arrays;
+
+import java.util.Scanner;
+
+/*
+	MAXIMUM PRODUCT SUBARRAY
+	(This is like Kadane’s Algorithm, but with multiplication instead of addition)
+	You are given an array.
+
+	Find the continuous subarray whose product is maximum.
+
+	Example:
+	[2, 3, -2, 4]
+
+	Best subarray:
+	[2, 3]
+
+	Product:
+	2 × 3 = 6  ✅ (maximum)
+ */
+public class _H_8_Maximum_Product_Subarray {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+
+
+		System.out.println("Enter the array size:");
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+
+		System.out.println("Enter the array elements:");
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int maxProd = arr[0];
+		int minProd = arr[0];
+		int result = arr[0];
+
+		for(int i =1;i<n;i++) {
+			int tempMax = Math.max(arr[i], Math.max(maxProd * arr[i] , minProd * arr[i]));
+			int tempMin = Math.min(arr[i], Math.min(maxProd * arr[i] , minProd * arr[i]));
+
+			maxProd = tempMax;
+			minProd = tempMin;
+
+			result = Math.max(result, maxProd);
+		}
+
+		System.out.println("The maximum product subarray is:" + result);
+
+		sc.close();
+	}
+
+}
