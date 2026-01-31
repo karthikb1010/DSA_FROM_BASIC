1b0baadc40240e1de7211ff93990c157e8814baf
karthik <karthikb1010@gmail.com>
2026-01-31 12:48:01 +0530

    update

---
+++ src/_2_arrays/_Q_17_Repeating_Elements.java
@@ -0,0 +1,60 @@
+package _2_arrays;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+FIND ALL REPEATING ELEMENTS
+
+You are given an array.
+You must print all numbers that appear more than once.
+
+🧪 Example:
+Array = [1, 2, 3, 2, 4, 1, 5, 2]
+
+
+Repeating elements are:
+1 (appears twice)
+2 (appears three times)
+
+Output:
+1 2
+ */
+public class _Q_17_Repeating_Elements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the array size:");
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		System.out.println("Enter the array elements:");
+		for(int i=0;i<n;i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		HashMap<Integer ,Integer> map = new HashMap<>();
+		
+		for(int i=0;i<n;i++) {
+			if(map.containsKey(arr[i])) {
+				map.put(arr[i], map.get(arr[i])+1);
+			}
+			else {
+				map.put(arr[i], 1);
+			}
+		}
+		System.out.println("The Repeated numers are");
+		for(int key : map.keySet()) {
+			if(map.get(key) > 1) {
+			System.out.print(key + " ");
+		}
+			}
+		
+		
+		
+		sc.close();
+	}
+
+}
---
+++ src/_2_arrays/_R_18_Find_Symmetric_Pairs.java
@@ -0,0 +1,60 @@
+package _2_arrays;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+/*
+FIND SYMMETRIC PAIRS
+
+You are given pairs of numbers like this:
+(a, b)
+
+A symmetric pair exists if:
+(a, b) and (b, a)
+
+both are present.
+
+🧪 Example:
+Input pairs:
+
+(1, 2)
+(3, 4)
+(2, 1)
+(5, 6)
+(4, 3)
+
+
+Symmetric pairs are:
+
+(1, 2) and (2, 1)
+(3, 4) and (4, 3)
+
+ */
+public class _R_18_Find_Symmetric_Pairs {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter number of pairs:");
+		int n = sc.nextInt();
+
+		HashMap<Integer,Integer> map = new HashMap<>();
+
+		System.out.println("Enter the pair:");
+		for(int i=0;i<n;i++) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+
+
+			if(map.containsKey(b) && map.get(b) == a) {
+				System.out.println("symmetric pairs are: (" + a + ", " + b + ")");
+			}else {
+				map.put(a, b);
+			}
+
+		}
+		sc.close();
+	}
+
+}
