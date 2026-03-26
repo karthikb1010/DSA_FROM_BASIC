350124062239219bafb534aefc513f5df201c22b
karthik <karthikb1010@gmail.com>
2026-03-26 19:59:39 +0530

    Node.Java

--- src/_6_LinkedList/Node.java
+++ src/_6_LinkedList/Node.java
@@ -1,6 +1,7 @@
 package _6_LinkedList;
 
 // here instead of writing same class in every problem we write once in package and it automatically understands it no import it required
+//common for all linked list files
 class Node{
 	
 	int data;
